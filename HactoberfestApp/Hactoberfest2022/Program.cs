723eabea8df90c800ba633ba8a781b2712d636d9
Madushani Rodrigo <95022003+MadushaniR@users.noreply.github.com>
2022-10-04 20:21:55 +0530

    initial commit

---
+++ HactoberfestApp/Hactoberfest2022/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hactoberfest2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ HactoberfestApp/Hactoberfest2022/viewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hactoberfest2022
+{
+    public class viewModel
+    {
+      
+    }
+}
