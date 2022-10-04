dc7f95dcb9bce00834e89898dd8571faa932d2f8
Ishan-Chanuka <ishanchanuka4@gmail.com>
2022-10-04 21:02:32 +0530

    added add method

--- HactoberfestApp/Hactoberfest2022/Program.cs
+++ HactoberfestApp/Hactoberfest2022/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CalLibrary;
 
 namespace Hactoberfest2022
 {
@@ -6,7 +7,38 @@ namespace Hactoberfest2022
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter a number : ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a number : ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Select a Operation [+, -, *, /]");
+
+            char opt = char.Parse(Console.ReadLine());
+
+
+            switch (opt)
+            {
+                case '+':
+                    Console.WriteLine(Calculations.Addition(num1, num2));
+                    break;
+
+                case '-':
+                    Console.WriteLine("Not yet implemented");
+                    break;
+
+                case '*':
+                    Console.WriteLine("Not yet implemented");
+                    break;
+
+                case '/':
+                    Console.WriteLine("Not yet implemented");
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
