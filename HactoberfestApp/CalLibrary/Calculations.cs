ef0f0a3b67faf1f2bb53b5babb95618a4e803df9
Vinicius de Morais <viniciusdemoraisbh@gmail.com>
2022-10-04 15:50:06 -0300

    Contribuição Hacktoberfest - Adiciona Subitração

--- HactoberfestApp/CalLibrary/Calculations.cs
+++ HactoberfestApp/CalLibrary/Calculations.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CalLibrary
+﻿namespace CalLibrary
 {
     public class Calculations
     {
         public static double Addition(double num1, double num2)
         {
             return num1 + num2;
+        }
+
+        public static double Subtraction(double num1, double num2)
+        {
+            return num1 - num2;
         }
     }
 }
