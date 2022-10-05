d054bdef5828b5f53c99b0ffdb95734b8d255792
Vinicius de Morais <viniciusdemoraisbh@gmail.com>
2022-10-05 08:56:17 -0300

    Contribuição Hacktoberfest - Adiciona unit test para adição

---
+++ HactoberfestApp/Tests/CalculationsTests.cs
@@ -0,0 +1,27 @@
+using CalLibrary;
+
+using FluentAssertions;
+
+using Xunit;
+
+namespace Tests
+{
+	public class CalculationsTests
+	{
+
+		[Fact]
+		public void Given_Num1_And_Num2_When_Addition_Should_ReturnExpetedResult()
+		{
+			//Arrange
+			int num1 = 2;
+			int num2 = 3;
+			int expectedResult = 5;
+
+			//Act
+			double result = Calculations.Addition(num1, num2);
+
+			//Assert
+			result.Should().Be(expectedResult);
+		}
+	}
+}
