390263efe8e6384783980baa75bda94536948d83
Thilina Pathirage <62783923+Thilina-Pathirage@users.noreply.github.com>
2022-10-12 22:27:43 +0530

    Add multiplication to the class library

--- HactoberfestApp/CalLibrary/Calculations.cs
+++ HactoberfestApp/CalLibrary/Calculations.cs
@@ -11,5 +11,10 @@
         {
             return num1 - num2;
         }
+        
+        public static double Multiplication(double num1, double num2)
+        {
+            return num1 * num2;
+        }
     }
 }
