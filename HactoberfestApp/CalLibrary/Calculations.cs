845fa356821156921356c740d5e60d52e0f07003
tharaka dhanushka <tharakadhanushka123@gmail.com>
2022-10-12 23:22:40 +0530

    add method for division

--- HactoberfestApp/CalLibrary/Calculations.cs
+++ HactoberfestApp/CalLibrary/Calculations.cs
@@ -16,5 +16,10 @@
         {
             return num1 * num2;
         }
+		
+		public static double Division(double num1, double num2)
+        {
+            return num1 / num2;
+        }
     }
 }
