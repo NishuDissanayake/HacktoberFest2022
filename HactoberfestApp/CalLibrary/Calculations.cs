bc5a30885e2aeec52dca53ceeb7c972ea67c4301
Nishu Dissanayake <63721945+NishuDissanayake@users.noreply.github.com>
2022-10-13 15:31:40 +0530

    Update Calculations.cs

--- HactoberfestApp/CalLibrary/Calculations.cs
+++ HactoberfestApp/CalLibrary/Calculations.cs
@@ -17,9 +17,14 @@
             return num1 * num2;
         }
 		
-		public static double Division(double num1, double num2)
+	public static double Division(double num1, double num2)
         {
             return num1 / num2;
+        }
+	    
+	public static double Remainder(double num1, double num2)
+        {
+            return num1 % num2;
         }
     }
 }
